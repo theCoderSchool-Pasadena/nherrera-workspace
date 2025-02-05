2a8fb6c91a923525d143e9e445f4cd5950b01278
Raymond Liu <raymondliu4414@gmail.com>
2025-02-04 21:30:32 -0800

    Create main.py

---
+++ harry-potter-quiz/main.py
@@ -0,0 +1,21 @@
+print ("Welcome to the Harry Potter Quiz")
+answer1 = input("What decade does the series take place in?\n>")
+if answer1 == "1990s":
+  print ("correct")
+else:
+  print ("incorrect")
+  quit()
+
+answer2 = input("Who does he meet on platform and 3/4?\n>")
+if answer2 im ("Ron","Ronald","Ron Weasley"):
+  print ("correct")
+else:
+  print ("incorrect")
+  quit()
+
+answer3 = input("What's the first book called?\n>")
+if answer3 == "Harry Potter and the Sorcerer's Stone":
+  print ("correct")
+else:
+  print ("incorrect")
+  quit()
